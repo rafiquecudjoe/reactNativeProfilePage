9df91eead34961766e39cf0feb5519061165d92b
Rafique Adam Cudjoe <rafiqueacudjoe@gmail.com>
2021-09-23 09:15:31 +0000

    initail commit

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Image,Button,TouchableOpacity, Pressable } from "react-native";
+import profileimg from "./assets/raf.jpg";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={profileimg} style={styles.profile} />
+      <View style={styles.containerinfo}>
+        <Text style={styles.label}>School</Text>
+        <Text>CodeTrain Ghana</Text>
+      </View>
+      <View style={styles.containerinfo}>
+        <Text style={styles.label}>Email Address</Text>
+        <Text style={styles.info}>Rafiqueacudjoe@gmail.com</Text>
+      </View>
+      <View style={styles.containerinfo}>
+        <Text style={styles.editinfolabel} >Name</Text>
+        <Text style={styles.editinfo}>Rafique Adam Cudjoe</Text>
+      </View>
+      <View style={styles.containerinfo}>
+        <Text style={styles.editinfolabel}>Nick Name</Text>
+        <Text style={styles.editinfo}t>Raf</Text>
+      </View>
+      <View style={styles.containerinfo}>
+        <Text style={styles.editinfolabel}>Emergency Contact</Text>
+        <Text style={styles.editinfo}>+233 5444 13229</Text>
+      </View>
+      <Pressable style={styles.buttonContainer}>
+        <Text style={styles.btext}>UPDATE PROFILE</Text>
+        </Pressable>
+      
     </View>
   );
 }
@@ -14,8 +37,51 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "flex-start",
   },
+  profile: {
+    width: 120,
+    height: 120,
+    borderRadius: 100,
+    marginTop: 100,
+    marginBottom: 20,
+    alignSelf: "center",
+  },
+  containerinfo: {
+    marginTop: 10,
+    marginBottom: 10,
+    marginHorizontal: 50,
+  },
+  label: {
+    color: 'grey',
+    marginBottom:5
+
+  },
+  buttonContainer:{
+    borderRadius:100,
+    marginTop: 10,
+    marginBottom: 10,
+    marginHorizontal: 50,
+    backgroundColor: '#34b4eb',
+    height:50
+  },
+  editinfo: {
+    paddingBottom:5,
+    borderBottomWidth: 0.7,
+    borderColor: 'grey',
+    
+  },
+  editinfolabel: {
+    color:'#34b4eb' ,
+    marginBottom:5
+
+  },
+  btext: {
+    fontSize:16,
+    color: 'white',
+    alignSelf: 'center',
+    paddingTop:15
+  }
+ 
 });
